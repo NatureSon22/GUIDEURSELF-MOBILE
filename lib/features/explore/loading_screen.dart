90672398d03bfa656145625de07a6dad9d7c2739
Kenneth San Pedro <kennethsanpedro1108@gmail.com>
2025-03-22 15:28:33 +0800

    commit

--- lib/features/explore/loading_screen.dart
+++ lib/features/explore/loading_screen.dart
@@ -21,7 +21,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
     _universityFuture = fetchUniversityDetails();
 
     // Start a timer for 5 seconds
-    _timer = Timer(const Duration(seconds: 2), () {
+    _timer = Timer(const Duration(seconds: 6), () {
       // Navigate to another component after 5 seconds
       context.go('/virtual-tour'); // Replace with your desired route
     });
